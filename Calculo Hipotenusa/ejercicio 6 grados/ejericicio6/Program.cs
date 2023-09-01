02fb48c97c857966588f5f1247189141c99e5d77
sebasredes19 <sebastianespinosavalencia@hotmail.com>
2023-09-01 12:32:41 -0500

    ejercicio6

---
+++ Calculo Hipotenusa/ejercicio 6 grados/ejericicio6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius
+
+
+public class ejercicio6
+
+{
+    static void Main(String[] args)
+    {
+        
+
+        Console.WriteLine("Conversor de grados Fahrenheit a grados Celsius");
+        Console.Write("Ingresa la temperatura en grados Fahrenheit: ");
+
+        double fahrenheit = Convert.ToDouble(Console.ReadLine());
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        Console.WriteLine($"La temperatura en grados Celsius es: {celsius}");
+    }
+
+
+}
