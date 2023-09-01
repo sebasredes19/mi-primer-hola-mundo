2020e9ab888a3d7c658f8a8aab9118070874109b
sebasredes19 <sebastianespinosavalencia@hotmail.com>
2023-09-01 14:28:47 -0500

    Ejercicio7

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Construir un programa que pida un número y luego diga si este número es par o no
+
+
+
+
+public class Ejercicio7
+{
+
+    static void Main()
+    {
+        Console.Write("Ingresa un número: ");
+        int numero = Convert.ToInt16(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"El número {numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numero} es impar.");
+        }
+
+    }
+}
