954ce64b8d0f2d1804b6dee226b50405afa2789e
sebasredes19 <sebastianespinosavalencia@hotmail.com>
2023-09-06 20:34:11 -0500

    Priimera parte ejercicio 9

---
+++ Cual es el numero mayor/NumeroMayor/Program.cs
@@ -0,0 +1,33 @@
+﻿// Construir un programa que pida por pantalla 3 números y luego diga cuál es el mayor de los números ingresados.
+
+
+
+public class Ejercicio9
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("calcular el mayor numero de los 3");
+        Console.WriteLine("Ingresa tres numeros");
+
+        Console.Write("numero1: ");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("numero2: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("numero3: ");
+        double numero3 = Convert.ToDouble(Console.ReadLine());
+
+    }
+
+
+        {
+
+        static void Main(object[] args) { }
+
+        }
+ 
+
+}
