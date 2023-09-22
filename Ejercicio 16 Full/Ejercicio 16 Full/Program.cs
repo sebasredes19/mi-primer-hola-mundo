5f0e61a52dd4a5d485b223d488d1aebf6d88fc2e
sebasredes19 <sebastianespinosavalencia@hotmail.com>
2023-09-21 21:37:09 -0500

    comienzo ejercicio 16

--- Ejercicio 16 Full/Ejercicio 16 Full/Program.cs
+++ Ejercicio 16 Full/Ejercicio 16 Full/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Ejercicio10
+{
+
+    static void Main(string[] args)
+
+    {
+
+        //declaracion de varibles
+        decimal rendimientoMes, interes, bonoMes, ahorroMes, rendimientoAnual,
+               interesAnual, bonoAnual, ahorroAnual, sumaTotalNeta;
+        bool continuar = true;
+        int  mes;
+
+        const decimal TASA_MAX = (decimal)1.5;
+
+        Console.WriteLine("------Natillera Navideña-----\n");
+
+        for (mes = 1; mes <= 12; mes++)
+        {
+
+        }w
+
+    }
+
+}
